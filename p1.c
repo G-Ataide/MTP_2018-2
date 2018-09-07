2e82678489f8338497424d55d2ddaff18e591f7d
Gabriel Cardoso <gcm.ataide@gmail.com>
2018-09-07 20:42:45 -0300

    Programa 1
    Primeira versão do Programa 1, funcionando com sucesso

---
+++ p1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+  char bits_in[256]={0};
+  char bits[256]={0};
+  int h=0,j=0,i=0,estado=0;
+  scanf("%s",bits_in);
+  for(;bits_in[h]!='\0';h++){
+    if((bits_in[h]=='0')||(bits_in[h]=='1')){
+      bits[j] = bits_in[h];
+      j++;
+    }
+  }
+  for(;bits[i]!='\0';i++){
+    if(estado==0){
+      if(bits[i]=='1'){
+        estado=1;
+      }
+    }else if(estado==1){
+      if(bits[i]=='1'){
+        estado=0;
+      }else if(bits[i]=='0'){
+        estado=2;
+      }
+    }else if(estado==2){
+      if(bits[i]=='0'){
+        estado=1;
+      }
+    }
+  }
+  if(estado==0 && i>=1){
+    printf("Multiplo");
+  }else if((estado==1 || estado==2) && i>=1){
+    printf("Nao Multiplo");
+  }
+
+}
