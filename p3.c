2585030c4406e1ab13cdcbc625d8383b98b57516
Gabriel Cardoso <gcm.ataide@gmail.com>
2018-10-10 09:04:21 -0300

    programa completo

---
+++ p3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+long long int eleva(int base, int expo){
+  int result=1;
+  for(;expo>0;expo--){
+    result*=base;
+  }
+  return result;
+}
+
+int main(){
+  int i,j=0,k;
+  unsigned long long int num=0;
+  char str[256];
+  scanf("%s",str);
+  for(k=0;str[k]!='\0';k++);
+  for(;k>=0;k--){
+    if(str[k]>=48 && str[k]<=57){
+      num+=(str[k]-48)*(eleva(10,j));
+      j++;
+    }
+  }
+  printf("%lld",num);
+}
