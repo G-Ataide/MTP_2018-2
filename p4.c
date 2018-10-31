2851e8976fc34d89e62ea066c84b8550e7448dc4
Gabriel Cardoso <gcm.ataide@gmail.com>
2018-10-31 09:57:15 -0300

    Identificação adicionada

--- p4.c
+++ p4.c
@@ -1,13 +1,16 @@
+/* p4.c */
+/* Gabriel Cardoso Mendes de Ataide */
+/* 11811ECP008 */
 #include <stdio.h>
-#include <stdlib.h>
 
 int ackermann(int m, int n){
   if(m==0){return n+1;}
-  if(n==0){return ackermann(m-1,1);}
-  if(n>0){return ackermann(m-1,ackermann(m,n-1));}
+  if(n==0){ackermann(m-1,1);}
+  if(n>0){ackermann(m-1,ackermann(m,n-1));}
 }
 
 int main(){
   int m,n;
   scanf("%i %i",&m,&n);
+  printf("%i",ackermann(m,n));
 }
